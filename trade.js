22b857aeaa098ea0eb217f524cc45e21d9cda188
HirokiTahara <renrenkiiro2@gmail.com>
2017-01-23 20:16:47 +0900

    add new file

--- trade.js
+++ trade.js
@@ -19,13 +19,13 @@ var sell_price = 0;
 
 //bitflyer zaifからjson取得
 var bitflyer = function  (zaif) {
-var path = '/v1/getticker';
-var query = '';
-var url = 'https://api.bitflyer.jp' + path + query;
-//request関数実行後にやりたい処理を第二引数のcallbackの中に書く
-request(url, function (err, response, payload) {
-        if (err === null) {
-                    try {
+  var path = '/v1/getticker';
+  var query = '';
+  var url = 'https://api.bitflyer.jp' + path + query;
+  //request関数実行後にやりたい処理を第二引数のcallbackの中に書く
+  request(url, function (err, response, payload) {
+    if (err === null) {
+      try {
         // エラーじゃなければJSONをパース
         var obj = JSON.parse(payload);
         //オブジェクトから買い気配取り出し
@@ -37,241 +37,242 @@ request(url, function (err, response, payload) {
         bitflyer_ask = obj.best_ask;
         bitflyer_ask_size = obj.best_ask_size;
 
-        }
-            catch (e) {
-                       err = e;
-                    }
-    	}
+      }
+      catch (e) {
+        err = e;
+      }
+    }
 
-    	if (err) {
-    		console.log(err);
-    	}
+    if (err) {
+      console.log(err);
+    }
 
-        zaif();
+    zaif();
 
-     });
-   };
+  });
+};
 
 //zaifからjson取得
 var zaif = function () {
-var path = '/api/1/depth/btc_jpy';
-var query = '';
-var url = 'https://api.zaif.jp' + path + query;
-    request(url, function (err, response, payload) {
-        if (err === null) {
-       try {
-                // エラーじゃなければJSONをパース
-                // 「JSON 文字列」から「JavaScript のオブジェクト」に変換する
-                obj = JSON.parse(payload);
-                //買い価格
-                zaif_bid = obj.bids[0][0];
-                zaif_bid_size = obj.bids[0][1];
-                //売り価格
-                zaif_ask = obj.asks[0][0];
-                zaif_ask_size = obj.asks[0][1];
+  var path = '/api/1/depth/btc_jpy';
+  var query = '';
+  var url = 'https://api.zaif.jp' + path + query;
+  request(url, function (err, response, payload) {
+    if (err === null) {
+      try {
+        // エラーじゃなければJSONをパース
+        // 「JSON 文字列」から「JavaScript のオブジェクト」に変換する
+        obj = JSON.parse(payload);
+        //買い価格
+        zaif_bid = obj.bids[0][0];
+        zaif_bid_size = obj.bids[0][1];
+        //売り価格
+        zaif_ask = obj.asks[0][0];
+        zaif_ask_size = obj.asks[0][1];
 
 
-                zaif_price = zaif_bid;
+        zaif_price = zaif_bid;
 
-           }
-            catch (e) {
-                        err = e;
-                       }
-    	}
+      }
+      catch (e) {
+        err = e;
+      }
+    }
 
-    	if (err) {
-                    console.log(err);
-                 }
-    });
+    if (err) {
+      console.log(err);
+    }
+  });
 };
 
 //注文処理
 setInterval (function(){
 
-            bitflyer(zaif);
-            //買い注文
-        if(buy_count === 0 && zaif_ask !== 0 && bitflyer_ask !== 0 && zaif_ask - bitflyer_ask >= 20){
+  bitflyer(zaif);
+  //買い注文
+  if(buy_count === 0 && zaif_ask !== 0 && bitflyer_ask !== 0 && zaif_ask - bitflyer_ask >= 20){
 
-               try {
-                  console.log("買い"+bitflyer_ask);
-                    var timestamp = Date.now().toString();
-                    var method = 'POST';
-                    var path = '/v1/me/sendchildorder';
-                    var body = JSON.stringify({
-                      "product_code": "BTC_JPY",
-                      "child_order_type": "LIMIT",//MARKET or LIMIT
-                      "side": "BUY",
-                      "price": bitflyer_ask,
-                      "size": 0.045,
-                      "minute_to_expire": 10000,
-                      "time_in_force": "GTC"
-                    });
+    try {
+      console.log("買い"+bitflyer_ask);
+      var timestamp = Date.now().toString();
+      var method = 'POST';
+      var path = '/v1/me/sendchildorder';
+      var body = JSON.stringify({
+        "product_code": "BTC_JPY",
+        "child_order_type": "LIMIT",//MARKET or LIMIT
+        "side": "BUY",
+        "price": bitflyer_ask,
+        "size": 0.045,
+        "minute_to_expire": 10000,
+        "time_in_force": "GTC"
+      });
 
-                    var text = timestamp + method + path + body;
-                    var sign = crypto.createHmac('sha256', secret).update(text).digest('hex');
+      var text = timestamp + method + path + body;
+      var sign = crypto.createHmac('sha256', secret).update(text).digest('hex');
 
-                    var options = {
-                        url: 'https://api.bitflyer.jp' + path,
-                        method: method,
-                        body: body,
-                        headers: {
-                            'ACCESS-KEY': key,
-                            'ACCESS-TIMESTAMP': timestamp,
-                            'ACCESS-SIGN': sign,
-                            'Content-Type': 'application/json'
-                        }
-                    };
-                    request(options, function (err, response, payload) {
-                        console.log(payload);
-                    });
-
-                    //購入価格の保存
-                    buy_price = bitflyer_ask;
-                    buy_count++;
-
-                }
-
-
-                  catch (e) {
-                        err = e;
-                       }
+      var options = {
+        url: 'https://api.bitflyer.jp' + path,
+        method: method,
+        body: body,
+        headers: {
+          'ACCESS-KEY': key,
+          'ACCESS-TIMESTAMP': timestamp,
+          'ACCESS-SIGN': sign,
+          'Content-Type': 'application/json'
         }
+      };
+      request(options, function (err, response, payload) {
+        console.log(payload);
+      });
 
-        //買い決済処理
-       else if(buy_count ===1&& buy_price < bitflyer_bid ){
-             try {
+      //購入価格の保存
+      buy_price = bitflyer_ask;
+      buy_count++;
 
-                    console.log("買い決済（売り）"+bitflyer_bid);
-                    var timestamp = Date.now().toString();
-                    var method = 'POST';
-                    var path = '/v1/me/sendchildorder';
-                    var body = JSON.stringify({
-                      "product_code": "BTC_JPY",
-                      "child_order_type": "LIMIT",
-                      "side": "SELL",
-                      "price": bitflyer_bid,
-                      "size": 0.045,
-                      "minute_to_expire": 10000,
-                      "time_in_force": "GTC"
-                    });
-
-                    var text = timestamp + method + path + body;
-                    var sign = crypto.createHmac('sha256', secret).update(text).digest('hex');
-
-                    var options = {
-                        url: 'https://api.bitflyer.jp' + path,
-                        method: method,
-                        body: body,
-                        headers: {
-                            'ACCESS-KEY': key,
-                            'ACCESS-TIMESTAMP': timestamp,
-                            'ACCESS-SIGN': sign,
-                            'Content-Type': 'application/json'
-                        }
-                    };
-                    request(options, function (err, response, payload) {
-                        console.log(payload);
-                    });
-                    buy_count--;
-                    buy_price = 0;
-             }
-              catch (e) {
-                        err = e;
-                       }
-
-       }
-
-        //売り注文
-         if(sell_count === 0 && zaif_bid !== 0 && bitflyer_bid !== 0 && bitflyer_bid - zaif_bid >= 20){
-
-               try {
-                   console.log("売り"+bitflyer_bid);
-                    var timestamp = Date.now().toString();
-                    var method = 'POST';
-                    var path = '/v1/me/sendchildorder';
-                    var body = JSON.stringify({
-                      "product_code": "BTC_JPY",
-                      "child_order_type": "LIMIT",
-                      "side": "SELL",
-                      "price": bitflyer_bid,
-                      "size": 0.045,
-                      "minute_to_expire": 10000,
-                      "time_in_force": "GTC"
-                    });
-
-                    var text = timestamp + method + path + body;
-                    var sign = crypto.createHmac('sha256', secret).update(text).digest('hex');
-
-                    var options = {
-                        url: 'https://api.bitflyer.jp' + path,
-                        method: method,
-                        body: body,
-                        headers: {
-                            'ACCESS-KEY': key,
-                            'ACCESS-TIMESTAMP': timestamp,
-                            'ACCESS-SIGN': sign,
-                            'Content-Type': 'application/json'
-                        }
-                    };
-                    request(options, function (err, response, payload) {
-                        console.log(payload);
-                    });
-
-                    //売却価格の保存
-                    sell_price = bitflyer_bid;
-                    sell_count++;
-
-                }
+    }
 
 
-                  catch (e) {
-                        err = e;
-                       }
+    catch (e) {
+      err = e;
+    }
+  }
+
+  //買い決済処理
+  else if(buy_count ===1&& buy_price < bitflyer_bid ){
+    try {
+
+      console.log("買い決済（売り）"+bitflyer_bid);
+      var timestamp = Date.now().toString();
+      var method = 'POST';
+      var path = '/v1/me/sendchildorder';
+      var body = JSON.stringify({
+        "product_code": "BTC_JPY",
+        "child_order_type": "LIMIT",
+        "side": "SELL",
+        "price": bitflyer_bid,
+        "size": 0.045,
+        "minute_to_expire": 10000,
+        "time_in_force": "GTC"
+      });
+
+      var text = timestamp + method + path + body;
+      var sign = crypto.createHmac('sha256', secret).update(text).digest('hex');
+
+      var options = {
+        url: 'https://api.bitflyer.jp' + path,
+        method: method,
+        body: body,
+        headers: {
+          'ACCESS-KEY': key,
+          'ACCESS-TIMESTAMP': timestamp,
+          'ACCESS-SIGN': sign,
+          'Content-Type': 'application/json'
         }
+      };
+      request(options, function (err, response, payload) {
+        console.log(payload);
+      });
+      buy_count--;
+      buy_price = 0;
+    }
+    catch (e) {
+      err = e;
+    }
 
-        //売り決済処理
-       else if(sell_count ===1 && bitflyer_ask < sell_price){
-            try {
+  }
 
-                    console.log("売り決済（買い）"+bitflyer_ask);
-                    var timestamp = Date.now().toString();
-                    var method = 'POST';
-                    var path = '/v1/me/sendchildorder';
-                    var body = JSON.stringify({
-                      "product_code": "BTC_JPY",
-                      "child_order_type": "LIMIT",
-                      "side": "BUY",
-                      "price": bitflyer_ask,
-                      "size": 0.045,
-                      "minute_to_expire": 10000,
-                      "time_in_force": "GTC"
-                    });
+  //売り注文
+  if(sell_count === 0 && zaif_bid !== 0 && bitflyer_bid !== 0 && bitflyer_bid - zaif_bid >= 20){
 
-                    var text = timestamp + method + path + body;
-                    var sign = crypto.createHmac('sha256', secret).update(text).digest('hex');
+    try {
+      console.log("売り"+bitflyer_bid);
+      var timestamp = Date.now().toString();
+      var method = 'POST';
+      var path = '/v1/me/sendchildorder';
+      var body = JSON.stringify({
+        "product_code": "BTC_JPY",
+        "child_order_type": "LIMIT",
+        "side": "SELL",
+        "price": bitflyer_bid,
+        "size": 0.045,
+        "minute_to_expire": 10000,
+        "time_in_force": "GTC"
+      });
 
-                    var options = {
-                        url: 'https://api.bitflyer.jp' + path,
-                        method: method,
-                        body: body,
-                        headers: {
-                            'ACCESS-KEY': key,
-                            'ACCESS-TIMESTAMP': timestamp,
-                            'ACCESS-SIGN': sign,
-                            'Content-Type': 'application/json'
-                        }
-                    };
-                    request(options, function (err, response, payload) {
-                        console.log(payload);
-                    });
-                    sell_price = 0;
-                    sell_count --;
-                }
+      var text = timestamp + method + path + body;
+      var sign = crypto.createHmac('sha256', secret).update(text).digest('hex');
 
-                 catch (e) {
-                        err = e;
-                       }
-}
-//終わり
+      var options = {
+        url: 'https://api.bitflyer.jp' + path,
+        method: method,
+        body: body,
+        headers: {
+          'ACCESS-KEY': key,
+          'ACCESS-TIMESTAMP': timestamp,
+          'ACCESS-SIGN': sign,
+          'Content-Type': 'application/json'
+        }
+      };
+      request(options, function (err, response, payload) {
+        console.log(payload);
+      });
 
- },200);
+      //売却価格の保存
+      sell_price = bitflyer_bid;
+      sell_count++;
+
+    }
+
+
+    catch (e) {
+      err = e;
+    }
+  }
+
+  //売り決済処理
+  else if(sell_count ===1 && bitflyer_ask < sell_price){
+    try {
+
+      console.log("売り決済（買い）"+bitflyer_ask);
+      var timestamp = Date.now().toString();
+      var method = 'POST';
+      var path = '/v1/me/sendchildorder';
+      var body = JSON.stringify({
+        "product_code": "BTC_JPY",
+        "child_order_type": "LIMIT",
+        "side": "BUY",
+        "price": bitflyer_ask,
+        "size": 0.045,
+        "minute_to_expire": 10000,
+        "time_in_force": "GTC"
+      });
+
+      var text = timestamp + method + path + body;
+      var sign = crypto.createHmac('sha256', secret).update(text).digest('hex');
+
+      var options = {
+        url: 'https://api.bitflyer.jp' + path,
+        method: method,
+        body: body,
+        headers: {
+          'ACCESS-KEY': key,
+          'ACCESS-TIMESTAMP': timestamp,
+          'ACCESS-SIGN': sign,
+          'Content-Type': 'application/json'
+        }
+      };
+      request(options, function (err, response, payload) {
+        console.log(payload);
+      });
+      sell_price = 0;
+      sell_count --;
+    }
+
+    catch (e) {
+      err = e;
+    }
+  }
+  //終わり
+  //  ブランチ変更テスト　インデント修正
+
+},200);
